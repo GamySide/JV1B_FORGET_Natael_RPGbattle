a27f8952adc5991d80ae458a36a3998d750d57f5
GamySide <nataelforget@gmail.com>
2022-11-29 21:10:57 +0100

    ...

--- code/scene-de-combat.js
+++ code/scene-de-combat.js
@@ -1,8 +1,16 @@
-function statistique (pv,atk,mana) {
-    var pv = pvCible;
-    var atk = atkCible;
-    var mana = manaCible;
-}
+let dialogue = document.getElementById("dialogue");
+let atkInteraction = document.getElementById("atkInteraction");
+let defInteraction = document.getElementById("defInteraction");
+let skillInteraction = document.getElementById("skillInteraction");
+let renStatistique = document.getElementById("renStatistique");
 
 
+
+var dogenadonPv = document.getElementById("dogenadonPv");
+var katculubPv = document.getElementById("katculubPv");
+var bunarok = document.getElementById("bunarokPv");
+var renPv = document.getElementById("renPv");
+renPv.value
+renStatistique.style.display = "block";
+renPv = 5;
 
