240d22dc95b8213256817161171e12b8879d3b98
GamySide <nataelforget@gmail.com>
2022-12-01 13:32:55 +0100

    avancement js

--- code/scene-de-combat.js
+++ code/scene-de-combat.js
@@ -1,12 +1,24 @@
-var renPv= document.getElementById("renPv").value;
-var ragisPv= document.getElementById("ragisPv").value;
-var alliaPv= document.getElementById("alliaPv").value;
-var succubisPv= document.getElementById("succubisPv").value;
+var renPv = document.getElementById("renPv").value;
+var ragisPv = document.getElementById("ragisPv").value;
+var alliaPv = document.getElementById("alliaPv").value;
+var succubisPv = document.getElementById("succubisPv").value;
+var renMana = document.getElementById("renMana").value;
+var ragisMana = document.getElementById("ragisMana").value;
+var alliaMana = document.getElementById("alliaMana").value;
+var succubisMana = document.getElementById("succubisMana").value;
+var dogenadonPv = document.getElementById("dogenadonPv").value;
+var katculubPv = document.getElementById("katculubPv").value;
+var bunarokPv = document.getElementById("bunarokPv").value;
+
 console.log(renPv);
 console.log(ragisPv);
 console.log(alliaPv);
 console.log(succubisPv);
+console.log(renMana);
+console.log(ragisMana);
+console.log(alliaMana);
+console.log(succubisMana);
 
-do
+/*/do
     renStatistique.style.display = "block";
-while(( renPv > 0 , ragisPv > 0 , alliaPv > 0 , succubisPv > 0 )||( dogenadon > 0 , katculub > 0 , bunarok > 0));
+while(( renPv > 0 , ragisPv > 0 , alliaPv > 0 , succubisPv > 0 )||( dogenadon > 0 , katculub > 0 , bunarok > 0));*/
