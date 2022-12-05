401a68681f608c33de073487ecffc9850f5e1548
GamySide <nataelforget@gmail.com>
2022-12-05 13:37:04 +0100

    Update scene-de-combat.js

--- code/scene-de-combat.js
+++ code/scene-de-combat.js
@@ -9,6 +9,17 @@ var succubisMana = document.getElementById("succubisMana").value;
 var dogenadonPv = document.getElementById("dogenadonPv").value;
 var katculubPv = document.getElementById("katculubPv").value;
 var bunarokPv = document.getElementById("bunarokPv").value;
+var renAtk = 20;
+var ragisAtk = 30;
+var alliaAtk = 10;
+var succubisAtk = 10;
+var dogenadonAtk = 20;
+var katculubAtk = 20;
+var bunarokAtk = 20;
+var renDef = 10;
+var ragisDef = 10;
+var alliaDef = 10;
+var succubisDef = 10;
 
 console.log(renPv);
 console.log(ragisPv);
@@ -18,6 +29,10 @@ console.log(renMana);
 console.log(ragisMana);
 console.log(alliaMana);
 console.log(succubisMana);
+
+renStatistique.style.displayb = "block";
+
+
 
 /*/do
     renStatistique.style.display = "block";
